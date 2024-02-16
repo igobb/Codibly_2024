14bb84af659d4a6d1d8e8189c7a540faaf341d2e
Tomasz Golab <t.golab06@gmail.com>
2024-02-16 15:26:12 +0100

    Reset all styles and content of application

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
-module.exports = {
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended'],
-    plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
+export default {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  };
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
