4b15706a11c4947648b02d6911623c886ffdc758
Tomasz Golab <t.golab06@gmail.com>
2024-02-18 02:11:36 +0100

    Adding ErrorView component

---
+++ src/components/ErrorView/ErrorView.tsx
@@ -0,0 +1,37 @@
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import { Fragment } from 'react';
+import { AxiosError } from 'axios';
+
+interface ErrorViewProps {
+  error: AxiosError;
+  filterId: string;
+}
+
+export const ErrorView = ({ error, filterId }: ErrorViewProps) => {
+  return (
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      sx={{ marginTop: 3, padding: 5, height: 270 }}
+    >
+      <Typography align="center" variant="subtitle1" gutterBottom>
+        {error?.response?.status === 404 && (
+          <Fragment>
+            There is no prodct with ID
+            <Typography sx={{ fontSize: '25px' }}>{filterId}</Typography>
+            in the database
+          </Fragment>
+        )}
+
+        {error?.response?.status !== 404 && <>An error has occurred</>}
+      </Typography>
+
+      <br />
+
+      <Typography align="center" variant="subtitle2" gutterBottom>
+        Status: {error?.response?.status}
+      </Typography>
+    </Stack>
+  );
+};
